6b1076af2d991658fa9aafc0ab01d56fb7313a59
MohammadAszadsadiq <143584712+MohammadAszadsadiq@users.noreply.github.com>
2023-12-15 16:13:12 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,3 +10,27 @@ resource "aws_instance" "jenkins_server" {
     }
     user_data=file("install.sh")
 }
+
+resource "null_resource" "perform_ebs_volume" {
+
+
+  # Connection Block for Provisioners to connect to EC2 Instance
+  connection {
+    type = "ssh"
+    host = tolist(aws_instance.jenkins_server[*].public_ip)[1]
+    user = "ubuntu"
+    private_key = file("mykey.pem")
+  }  
+ provisioner "remote-exec" {
+    inline = [
+      //"sudo chown -R jenkins:jenkins /var/lib/jenkins",
+      "sudo systemctl stop nginx",
+      "sudo lsblk",
+      "sudo mkfs -t  ext4 /dev/xvdh",
+      "sudo mount /dev/xvdh /usr/share/nginx/html",
+      "sudo systemctl start nginx"
+
+    ]
+  }
+
+}
