530c9b6c7a7ede1873987198c219d3568973f5f0
MohammadAszadsadiq <143584712+MohammadAszadsadiq@users.noreply.github.com>
2023-12-15 14:53:02 +0530

    Update provider.tf

---
+++ provider.tf
@@ -0,0 +1,8 @@
+# Please provide the cloud name
+provider "aws" {
+  //shared_config_files      = ["/home/ubuntu/.aws/conf"]
+  //shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+  //profile                  = "dev"
+  region = "${var.region}"
+ 
+}
