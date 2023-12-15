b8a3a19d1cb5dab7d80431f657affb73bdf2645c
aszadali <ma00595947@gmail.com>
2023-12-15 14:26:47 +0530

    terraform data

---
+++ ebs.tf
@@ -0,0 +1,51 @@
+# EBS volume creation
+/*
+resource "aws_ebs_volume" "docker_data" {
+    
+    availability_zone = "${data.aws_ec2_instance.get-az-name.availability_zone}"
+    size = 10
+    tags = {
+        Name="jenkins-dev-data"
+    }
+
+depends_on = [ data.aws_instance.get-az-name]
+}
+*/
+
+resource "aws_ebs_volume" "docker_data" {
+    
+    availability_zone = tolist(aws_instance.jenkins_server[*].availability_zone)[1]
+    size = 10
+    tags = {
+        Name="jenkins-dev-data"
+    }
+
+}
+
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.docker_data.id
+  instance_id = tolist(aws_instance.jenkins_server[*].id)[1]
+}
+
+
+
+resource "aws_ebs_snapshot" "jenkins_snapshot" {
+  volume_id = aws_ebs_volume.docker_data.id
+
+  tags = {
+    Name = "jenkins_data"
+  }
+  depends_on = [aws_ebs_volume.docker_data, aws_volume_attachment.ebs_att]
+}
+
+
+resource "aws_ebs_snapshot_copy" "jenkins_snap" {
+    source_snapshot_id = aws_ebs_snapshot.jenkins_snapshot.id
+    source_region = "ap-south-1"
+    tags= {
+        Name = "jenkins_snap"
+    }
+  depends_on = [ aws_ebs_volume.docker_data, aws_volume_attachment.ebs_att,aws_ebs_snapshot.jenkins_snapshot]
+}
---
+++ ec2.tf
@@ -0,0 +1,35 @@
+# Resource creation for jenkins installation
+resource "aws_instance" "jenkins_server" { 
+    ami = "ami-06aa3f7caf3a30282"
+    instance_type = "${var.instance_size}"
+    key_name = "sbfsajbj"
+    count = 3
+    tags = {
+      Name="jenkins_machine-${count.index + 1}"
+    }
+    user_data=file("install.sh")
+}
+
+
+resource "null_resource" "perform_ebs_volume" {
+
+
+  # Connection Block for Provisioners to connect to EC2 Instance
+  connection {
+    type = "ssh"
+    host = tolist(aws_instance.jenkins_server[*].public_ip)[1]
+    user = "ubuntu"
+    private_key = file("mykey.pem")
+  }  
+ provisioner "remote-exec" {
+    inline = [
+      "sudo systemctl stop jenkins",
+      "sudo lsblk",
+      "sudo mkfs -t  ext4 /dev/xvdh",
+      "sudo mount /dev/xvdh /var/lib/jenkins",
+      "sudo chown -R jenkins:jenkins /var/lib/jenkins",
+      "sudo systemctl start jenkins"
+
+    ]
+  }
+}
